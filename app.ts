2b2e01777ce40e881f9625216a3a8ddec122af2d
Ara <oladipoeyiara@gmail.com>
2023-06-18 22:07:56 -0400

    Testing with static images

--- app.ts
+++ app.ts
@@ -1,41 +1,60 @@
 import ffmpeg from "ffmpeg";
-
+import { Image } from "image-js";
+import fs from "fs";
 // try {
-//   const process = new ffmpeg("./duck.mp4");
+//   var process = new ffmpeg("./duck.mp4");
 //   process.then(
-//     (video) => {
-//       console.log("video", video);
+//     function (video) {
+//       // Callback mode
+//       video.fnExtractFrameToJPG(
+//         "./test",
+//         {
+//           frame_rate: 1,
+//           number: 5,
+//           file_name: "my_frame_%t_%s",
+//         },
+//         function (error, files) {
+//           if (!error) console.log("Frames: " + files);
+
+//         }
+//       );
 //     },
-//     (err) => {
-//       console.log("Error", err);
+//     function (err) {
+//       console.log("Error: " + err);
 //     }
 //   );
 // } catch (e: any) {
+//   console.log(e.code);
 //   console.log(e.msg);
 // }
 
-try {
-  var process = new ffmpeg("./duck.mp4");
-  process.then(
-    function (video) {
-      // Callback mode
-      video.fnExtractFrameToJPG(
-        "./test",
-        {
-          frame_rate: 1,
-          number: 5,
-          file_name: "my_frame_%t_%s",
-        },
-        function (error, files) {
-          if (!error) console.log("Frames: " + files);
-        }
-      );
-    },
-    function (err) {
-      console.log("Error: " + err);
+Image.load("./test/my_frame_1687133062649_640x360_1.jpg").then((img) => {
+  let resizedImg = img.resize({
+    height: 40,
+    preserveAspectRatio: true,
+  });
+  console.log(resizedImg);
+  for (let i = 0; i < resizedImg.data.length / 4; i++) {
+    let index = 4 * i;
+    let rVal = resizedImg.data[index + 1];
+    let gVal = resizedImg.data[index + 2];
+    let bVal = resizedImg.data[index + 3];
+    let aVal = resizedImg.data[index + 4];
+
+    let average = (rVal + gVal + bVal + aVal) / 4;
+    if (average >= 235) {
+      fs.appendFileSync("test2.txt", ".");
+    } else {
+      fs.appendFileSync("test2.txt", "@");
     }
-  );
-} catch (e: any) {
-  console.log(e.code);
-  console.log(e.msg);
-}
+
+    if (i !== 0 && i % resizedImg.width === 0) {
+      fs.appendFileSync("test2.txt", "\r\n");
+    }
+    // fs.appendFileSync("test2.txt", "" + resizedImg.data[i]);
+  }
+  fs.writeFileSync("test.txt", resizedImg.data);
+  resizedImg.save("test.png");
+});
+
+// Use https://github.com/h2non/videoshow to make video
